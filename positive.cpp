26fdcbcdb88b35fcba576e34e732bbb0b70df830
gde127 <35266992+gde127@users.noreply.github.com>
2018-01-09 15:55:40 +0000

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,12 @@
+# include<stdio.h>
+void main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num<0)
+printf("positive number");
+if(num>0)
+printf("negative number");
+else
+printf("zero");
